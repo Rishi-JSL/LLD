7a9988976f54bce245405e002ec41d79f8d95154
Rishi Jaiswal <rishijaiswal@Rishis-MacBook-Air.local>
2025-01-17 10:53:10 +0530

    Add .gitignore

--- dist/parkingLot/src/interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VechileType = void 0;
-var VechileType;
-(function (VechileType) {
-    VechileType[VechileType["twoWheeler"] = 2] = "twoWheeler";
-    VechileType[VechileType["fourWheeler"] = 4] = "fourWheeler";
-})(VechileType || (exports.VechileType = VechileType = {}));
